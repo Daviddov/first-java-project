5a31d9b75da5fab487b3c2ed0a55a9aa7dcb2aca
DavidDov <109440152+Daviddov@users.noreply.github.com>
2023-01-02 14:29:14 +0200

    7

---
+++ src/first_java_project/Java_5.java
@@ -0,0 +1,26 @@
+package first_java_project;
+
+public class Java_5 {
+
+public static void main (String[] args) {
+//	int num = 7495, digit, multiple = 1, newNum = 0 ;
+//	while (num != 0 ) {
+//		digit = num  % 10;
+//		digit++;
+//		digit *= multiple;
+//		multiple *= 10;
+//		newNum += digit; 
+//		num /= 10;
+//	}
+//	System.out.print(newNum);
+	
+//	String s = "bambalik";
+//	while(s.length() > 0) {
+//		System.out.print(s.charAt(s.length()-1));
+//		s = s.substring(0, s.length()-1);
+//	}
+	
+	String st1 ="aba", st2 = "daba";
+	System.out.println(st1.compareTo(st2));
+}
+}
